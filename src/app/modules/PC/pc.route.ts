f1ce1d6c8f93e004ef3da4242f8d9ce71eefd775
sarwar-asik <sarwarasik@gmail.com>
2023-07-28 15:09:58 +0600

    validation added

--- src/app/modules/PC/pc.route.ts
+++ src/app/modules/PC/pc.route.ts
@@ -1,9 +1,16 @@
-import express from "express"
-import { PCController } from "./pc.controller"
+import express from 'express';
+import { PCController } from './pc.controller';
+import validateRequest from '../../middlesWare/validateUserRequest';
+import { PCValidation } from './pc.validation';
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/',PCController.getALLPC)
-router.get('/:id',PCController.getSinglePC)
-router.delete('/:id',PCController.deletePC)
-export const  PCRouter = router
+router.get('/', PCController.getALLPC);
+router.post(
+  '/',
+  validateRequest(PCValidation.createPCZodSchema),
+  PCController.createPC
+);
+router.get('/:id', PCController.getSinglePC);
+router.delete('/:id', PCController.deletePC);
+export const PCRouter = router;
---
+++ src/app/modules/PC/pc.validation.ts
@@ -0,0 +1,58 @@
+import { z } from 'zod';
+
+const KeyFeaturesSchema = z.object({
+  Brand: z.string(),
+  Model: z.string(),
+  Specification: z.string(),
+  Socket: z.string(),
+  Cores: z.string(),
+  Threads: z.string(),
+  TDP: z.string(),
+});
+
+const createPCZodSchema = z.object({
+  body: z.object({
+    image: z.string({
+      required_error: 'image is required',
+    }),
+    productName: z.string({
+      required_error: 'productName is required',
+    }),
+    category: z.enum([
+      'CPU',
+      'Motherboard',
+      'RAM',
+      'Power Supply Unit',
+      'Storage Device',
+      'Monitor',
+    ]),
+    status: z.enum(['In Stock', 'Out of Stock'], {
+      required_error: 'status is required',
+    }),
+    price: z.number({
+      required_error: 'price is required',
+    }),
+    description: z.string({
+      required_error: 'description is required',
+    }),
+    keyFeatures: KeyFeaturesSchema,
+    individualRating: z.number({
+      required_error: 'individualRating is required',
+    }),
+    averageRating: z.number({
+      required_error: 'averageRating is required',
+    }),
+    reviews: z.array(
+      z.string({
+        required_error: 'review is required',
+      })
+    ),
+  }),
+});
+
+
+
+export const PCValidation = {
+  createPCZodSchema,
+
+};
